dacc8ce0bb5268fe8a11ddb6f69c68e3bf14c579
piguzovanton-ops <piguzovanton@gmail.com>
2025-09-24 18:16:24 +0300

    Update beach.py

--- beach.py
+++ beach.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randint
 
 #небо
 screen = turtle.Screen()
@@ -24,7 +25,7 @@ turtle.end_fill()
 
 #вода
 turtle.penup()
-turtle.goto(-300, 100)
+turtle.goto(-300 , 100)
 turtle.pendown()
 turtle.begin_fill()
 turtle.fillcolor("blue")
@@ -37,6 +38,22 @@ turtle.right(90)
 turtle.forward(100)
 turtle.right(90)
 turtle.end_fill()
+
+#волны
+turtle.penup()
+turtle.goto(-270 , -15)
+turtle.color('blue')
+for i in range(11):
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.fillcolor('blue')
+    turtle.circle(30)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.goto(-270 + 60*i, -15)
+
+turtle.color('black')
+
 
 #ствол пальмы
 turtle.penup()
@@ -142,12 +159,142 @@ turtle.forward(15)
 turtle.right(90)
 turtle.end_fill()
 
+
+
+
+
+#шезлонг
+t = turtle.Turtle()
+t.speed(3)
+t.pensize(2)
+xx = -130
+yy = -55
+t.penup()
+t.goto(xx, yy)
+t.pendown()
+
+
+t.color("red")
+t.begin_fill()
+t.goto(xx + 50, yy)  
+t.goto(xx + 45, yy + 15)  
+t.goto(xx + 5, yy + 15)  
+t.goto(xx, yy)  
+t.end_fill()
+
+
+t.penup()
+t.goto(xx + 5, yy + 15)
+t.pendown()
+t.color("darkorange")
+t.begin_fill()
+t.goto(xx + 10, yy + 35)  
+t.goto(xx - 5, yy + 35)  
+t.goto(xx, yy + 15)  
+t.goto(xx + 5, yy + 15)  
+t.end_fill()
+
+
+t.color("brown")
+t.pensize(3)
+
+
+t.penup()
+t.goto(xx, yy)
+t.pendown()
+t.goto(xx - 5, yy - 10)
+
+
+t.penup()
+t.goto(xx + 50, yy)
+t.pendown()
+t.goto(xx + 55, yy - 10)
+
+t.penup()
+t.goto(xx, yy + 15)
+t.pendown()
+t.goto(xx - 5, yy + 5)
+
+
+t.penup()
+t.goto(xx + 45, yy + 15)
+t.pendown()
+t.goto(xx + 40, yy + 5)
+
+
+t.color("black")
+t.pensize(1)
+
+
+for i in range(3):
+    y_pos = yy + 5 + i * 3
+    t.penup()
+    t.goto(xx + 2, y_pos)
+    t.pendown()
+    t.goto(xx + 43, y_pos)
+
+
+for i in range(4):
+    y_pos = yy + 18 + i * 4
+    t.penup()
+    t.goto(xx + 1, y_pos)
+    t.pendown()
+    t.goto(xx + 8, y_pos)
+
+
+
+#зонт
+
+umbrella_x = xx - 25
+umbrella_y = yy 
+
+
+t.pensize(4)
+t.color("brown")
+t.penup()
+t.goto(umbrella_x + 2, umbrella_y)
+t.pendown()
+t.goto(umbrella_x + 2, umbrella_y + 60)
+
+t.pensize(2)
+t.color("red")
+t.penup()
+t.goto(umbrella_x - 25, umbrella_y + 60)  
+t.pendown()
+
+
+t.begin_fill()
+t.setheading(90)  
+for i in range(180):  
+    t.forward(0.5) 
+    t.right(1)      
+t.end_fill()
+
+t.color("white")
+t.pensize(2)
+stripes_angles = [-30, -60, -90, -120, -150]  
+
+for angle in stripes_angles:
+    t.penup()
+    t.goto(umbrella_x + 3, umbrella_y + 89) 
+    t.pendown()
+    t.setheading(angle)  
+    t.forward(31)        
+
+
+
+#черепаха
 turtle.shape('turtle')
 turtle.penup()
-turtle.goto(-50 ,-50)
+turtle.goto(xx + 18 ,yy + 10)
+turtle.setheading(0)
 turtle.pendown()
 turtle.color('green')
-turtle.stamp()
-   
-#черепаха
+
+
+
+
+
+
+
 turtle.done() 
